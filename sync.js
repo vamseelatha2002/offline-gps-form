26b96047af4df7a70b9be029f68cb708287f198d
vamseelatha2002 <vamseelatha2002@gmail.com>
2025-06-10 16:27:16 +0530

    Update sync.js

--- sync.js
+++ sync.js
@@ -1,12 +1,7 @@
 const backendURL = "https://script.google.com/a/macros/datail.in/s/AKfycbw4fymgtBoaXbVJgIdrfww6WBdO1Y3CrkLZT2gZq97mWcLrUo2pCw9d_0uteq9tkLE/exec";
-console.log("Sync function started.");
 
 window.addEventListener("online", syncToServer);
-fetch(url, requestOptions)
-  .then(response => response.json())
-  .then(data => console.log("Google Forms Response:", data))
-  .catch(error => console.error("Error syncing data:", error));
-console.error("Something went wrong:", error);
+
 
 function syncToServer() {
   const openReq = indexedDB.open("activityFormDB", 1);
@@ -14,7 +9,7 @@ function syncToServer() {
     const db = openReq.result;
     const tx = db.transaction("submissions", "readwrite");
     const store = tx.objectStore("submissions");
-
+    console.log("Syncing data to Google Forms:", formData);
     const allData = store.getAll();
     allData.onsuccess = async () => {
       const records = allData.result;
